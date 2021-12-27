06dae0173fea98f3f54ca2686c26db32dee3ba43
Abdullah1738 <ardudeisepic@outlook.com>
2021-12-27 01:52:47 +0000

    New contract updates

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,24 @@ async fn main() -> web3::Result<()> {
     let web3 = web3::Web3::new(transport);
 
     //contract data. This will need to be changed when we deploy the contract / test contract etc
-    let nft_addr = Address::from_str("0xbd784Ed8e296ABe68e7E46F088e845838FB2bE53").unwrap();
+    let nft_addr = Address::from_str("0xFe7A13D085a099dB9daF50b803d2eB765c2E04df").unwrap();
     let contract = Contract::from_json(web3.eth(), nft_addr, include_bytes!("abi.json")).unwrap();
 
+
+    //change 0xc... to the address of the sender
+    let sender = Address::from_str("0xc0016f4AE265f7311B4B6991a7aafc4052A8d3E7").unwrap();
+
     //chnage 12 here to the tokenID
-    let result = contract.query("ownerOf", (U256::from((12))), None, Options::default(), None);
-    let owner_of: Address = result.await.unwrap();
-    println!("The owner of tokenID 12 is {:02X?}", owner_of);
+    let tokenID = U256::from(12);
+
+    
+    let result = contract.query("balanceOf", (sender,tokenID), None, Options::default(), None);
+    let owner_of: U256 = result.await.unwrap();
+    if owner_of == U256::from(1) {
+     println!("Is owner");
+    } else {
+        println!("Not owner")
+    }
 
     Ok(())
 }
